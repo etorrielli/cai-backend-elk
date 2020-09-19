b6a12bdbdd414f31b5ba0ea4ae67ce1dea2220ed
edgar <e.torrielli@gmail.com>
2020-09-19 13:51:51 -0300

    Prueba MongoClient

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # cai-backend
+## Consultas disponibles:
+* devolver todos los accidentes ocurridos entre 2 fechas dadas
+* determinar las condiciones más comunes en los accidentes (hora del día, condiciones climáticas, etc)
+* dado un punto geográfico y un radio (expresado en kilómetros) devolver todos los accidentes ocurridos dentro del radio.
+* devolver los 5 puntos más peligrosos (definiendo un determinado radio)
+* obtener la distancia promedio desde el inicio al fin del accidente
+
--- src/test/java/com/ekeepoit/cai/CaiApplicationTests.java
+++ src/test/java/com/ekeepoit/cai/CaiApplicationTests.java
@@ -1,15 +1,17 @@
 package com.ekeepoit.cai;
 
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.model.Filters;
+import org.bson.Document;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 
-import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.Date;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class CaiApplicationTests {
@@ -21,17 +23,31 @@ class CaiApplicationTests {
     void contextLoads() {
     }
 
-    @Test
-    public void testGetAccidentsByDates() {
-        ResponseEntity<Object[]> responseEntity = restTemplate.getForEntity("/api/accident/datefrom/2016-02-08/dateto/2016-02-09", Object[].class);
+//    @Test
+//    public void testGetAccidentsByDates() {
+//        ResponseEntity<Object[]> responseEntity = restTemplate.getForEntity("/api/accident/datefrom/2016-02-08/dateto/2016-02-09", Object[].class);
+//
+//        if (responseEntity.getStatusCode().equals(HttpStatus.OK)) {
+//            Object[] objects = responseEntity.getBody();
+//            System.out.println("cantidad: " + objects.length);
+//            // ArrayList<Object> accidentList = new ArrayList<Object>(Arrays.asList(objects));
+//            // accidentList.forEach(accident -> System.out.println(accident));
+//        } else {
+//            System.out.println(responseEntity.getStatusCode().value() + responseEntity.getStatusCode().getReasonPhrase());
+//        }
+//    }
 
-        if (responseEntity.getStatusCode().equals(HttpStatus.OK)) {
-            Object[] objects = responseEntity.getBody();
-            System.out.println("cantidad: " + objects.length);
-            // ArrayList<Object> accidentList = new ArrayList<Object>(Arrays.asList(objects));
-            // accidentList.forEach(accident -> System.out.println(accident));
-        } else {
-            System.out.println(responseEntity.getStatusCode().value() + responseEntity.getStatusCode().getReasonPhrase());
-        }
+    @Test
+    @SuppressWarnings("deprecation")
+    public void testGetAccidentsGroup() {
+        MongoClient mongoClient = MongoClients.create("mongodb://localhost:27017");
+        MongoDatabase database = mongoClient.getDatabase("accidentdb");
+
+        MongoCollection<Document> collection = database.getCollection("accident");
+
+        long antes = new Date().getTime();
+        System.out.println("cantidad " + collection.countDocuments(Filters.eq("State", "OH")));
+        long despues = new Date().getTime();
+        System.out.println("tiempo " + (despues - antes) + " milisegundos");
     }
 }
